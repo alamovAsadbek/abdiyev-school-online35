c99a0ef7a414a4d516a93e6ef3c320f8ae1c7d14
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2025-12-04 12:07:26 +0500

    changed

--- src/backend/apps/notifacations/apps.py
+++ src/backend/apps/notifacations/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NotifacationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notifacations'
+    name = 'apps.notifacations'
--- src/backend/apps/payments/admin.py
+++ src/backend/apps/payments/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from .models import Payment
-
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'category', 'amount', 'status', 'created_at']
-    list_filter = ['status', 'created_at']
-    search_fields = ['user__username', 'transaction_id']
-    readonly_fields = ['created_at', 'updated_at']
+# from django.contrib import admin
+# from .models import Payment
+#
+#
+# @admin.register(Payment)
+# class PaymentAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'user', 'category', 'amount', 'status', 'created_at']
+#     list_filter = ['status', 'created_at']
+#     search_fields = ['user__username', 'transaction_id']
+#     readonly_fields = ['created_at', 'updated_at']
