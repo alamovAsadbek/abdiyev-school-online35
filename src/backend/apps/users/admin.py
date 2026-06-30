6970ccc5cda00b1e8988ad96fff9d44d9f632382
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2026-06-30 10:12:05 +0500

    changed;

--- src/backend/apps/users/admin.py
+++ src/backend/apps/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+admin.site.site_header = 'ABDIYEV SCHOOL ADMIN PANEL'
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -9,19 +11,19 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ['role', 'is_blocked', 'is_active', 'created_at']
     search_fields = ['username', 'email', 'phone']
     ordering = ['-created_at']
-    
+
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'avatar')}),
         ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'is_blocked')}),
         ('Important dates', {'fields': ('last_login', 'date_joined', 'created_at')}),
     )
-    
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', 'password1', 'password2', 'role'),
         }),
     )
-    
+
     readonly_fields = ['created_at', 'date_joined', 'last_login']
