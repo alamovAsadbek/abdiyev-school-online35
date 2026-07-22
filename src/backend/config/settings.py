05de7384a0378b73f1815590e5a308e236fca2f2
Server Deploy <server@abdiyev.uz>
2026-07-22 06:42:01 +0000

    DB sozlamasini environment-based qilish (local=sqlite, server=postgres)

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -15,7 +15,29 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'abdiyev.uz',
+    'www.abdiyev.uz',
+    '127.0.0.1',
+    'localhost',
+    'backend',
+    '157.230.81.76',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://157.230.81.76',
+    'https://abdiyev.uz',
+    'https://www.abdiyev.uz',
+    'http://localhost:8080',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://abdiyev.uz',
+    'https://www.abdiyev.uz',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Application definition
 INSTALLED_APPS = [
@@ -88,12 +110,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DB_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME', 'abdiyev_db'),
+            'USER': os.environ.get('DB_USER', 'abdiyev_user'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
